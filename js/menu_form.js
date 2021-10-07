8b9309a7375ff7541593aef7b5ae3371a48a0886
AliakseiHryhoryeu <88304337+AliakseiHryhoryeu@users.noreply.github.com>
2021-10-07 13:58:26 +0300

    Load project at github

---
+++ js/menu_form.js
@@ -0,0 +1,197 @@
+/* Menu form start */
+$('#form_plus_rooms1').on('click', function(){
+    if($('#form_rooms1').html()==='1-комнатная'){
+    $('#form_rooms1').html('2-комнатная');
+}else if($('#form_rooms1').html()==='2-комнатная'){
+    $('#form_rooms1').html('3-комнатная');
+}else if($('#form_rooms1').html()==='3-комнатная'){
+    $('#form_rooms1').html('4-комнатная');
+}else if($('#form_rooms1').html()==='4-комнатная'){
+    $('#form_rooms1').html('5-комнатная');
+}else if($('#form_rooms1').html()==='5-комнатная'){
+    $('#form_rooms1').html('6-комнатная');
+}else if($('#form_rooms1').html()==='6-комнатная'){
+    $('#form_rooms1').html('7-комнатная');
+}else if($('#form_rooms1').html()==='7-комнатная'){
+    $('#form_rooms1').html('8-комнатная');
+}else if($('#form_rooms1').html()==='8-комнатная'){
+    $('#form_rooms1').html('9-комнатная');
+}else if($('#form_rooms1').html()==='9-комнатная'){
+    $('#form_rooms1').html('10-комнатная');
+}
+});
+
+$('#form_minus_rooms1').on('click', function(){
+    if($('#form_rooms1').html()==='2-комнатная'){
+    $('#form_rooms1').html('1-комнатная');
+}else if($('#form_rooms1').html()==='3-комнатная'){
+    $('#form_rooms1').html('2-комнатная');
+}else if($('#form_rooms1').html()==='4-комнатная'){
+    $('#form_rooms1').html('3-комнатная');
+}else if($('#form_rooms1').html()==='5-комнатная'){
+    $('#form_rooms1').html('4-комнатная');
+}else if($('#form_rooms1').html()==='6-комнатная'){
+    $('#form_rooms1').html('5-комнатная');
+}else if($('#form_rooms1').html()==='7-комнатная'){
+    $('#form_rooms1').html('6-комнатная');
+}else if($('#form_rooms1').html()==='8-комнатная'){
+    $('#form_rooms1').html('7-комнатная');
+}else if($('#form_rooms1').html()==='9-комнатная'){
+    $('#form_rooms1').html('8-комнатная');
+}else if($('#form_rooms1').html()==='10-комнатная'){
+    $('#form_rooms1').html('9-комнатная');
+}
+});
+
+$('#form_minus_bathrooms1').on('click', function(){
+    if($('#form_bathrooms1').html()==='2 санузла'){
+    $('#form_bathrooms1').html('1 санузел');
+}else if($('#form_bathrooms1').html()==='3 санузла'){
+    $('#form_bathrooms1').html('2 санузла');
+}else if($('#form_bathrooms1').html()==='4 санузла'){
+    $('#form_bathrooms1').html('3 санузла');
+}else if($('#form_bathrooms1').html()==='5 санузлов'){
+    $('#form_bathrooms1').html('4 санузла');
+}else if($('#form_bathrooms1').html()==='6 санузлов'){
+    $('#form_bathrooms1').html('5 санузлов');
+}
+});
+
+$('#form_plus_bathrooms1').on('click', function(){
+    if($('#form_bathrooms1').html()==='1 санузел'){
+    $('#form_bathrooms1').html('2 санузла');
+}else if($('#form_bathrooms1').html()==='2 санузла'){
+    $('#form_bathrooms1').html('3 санузла');
+}else if($('#form_bathrooms1').html()==='3 санузла'){
+    $('#form_bathrooms1').html('4 санузла');
+}else if($('#form_bathrooms1').html()==='4 санузла'){
+    $('#form_bathrooms1').html('5 санузлов');
+}else if($('#form_bathrooms1').html()==='5 санузлов'){
+    $('#form_bathrooms1').html('6 санузлов');
+}
+});
+
+
+$('#form_plus_rooms2').on('click', function(){
+    if($('#form_rooms2').html()==='1-комнатная'){
+    $('#form_rooms2').html('2-комнатная');
+}else if($('#form_rooms2').html()==='2-комнатная'){
+    $('#form_rooms2').html('3-комнатная');
+}else if($('#form_rooms2').html()==='3-комнатная'){
+    $('#form_rooms2').html('4-комнатная');
+}else if($('#form_rooms2').html()==='4-комнатная'){
+    $('#form_rooms2').html('5-комнатная');
+}else if($('#form_rooms2').html()==='5-комнатная'){
+    $('#form_rooms2').html('6-комнатная');
+}else if($('#form_rooms2').html()==='6-комнатная'){
+    $('#form_rooms2').html('7-комнатная');
+}else if($('#form_rooms2').html()==='7-комнатная'){
+    $('#form_rooms2').html('8-комнатная');
+}else if($('#form_rooms2').html()==='8-комнатная'){
+    $('#form_rooms2').html('9-комнатная');
+}else if($('#form_rooms2').html()==='9-комнатная'){
+    $('#form_rooms2').html('10-комнатная');
+}
+});
+
+$('#form_minus_rooms2').on('click', function(){
+    if($('#form_rooms2').html()==='2-комнатная'){
+    $('#form_rooms2').html('1-комнатная');
+}else if($('#form_rooms2').html()==='3-комнатная'){
+    $('#form_rooms2').html('2-комнатная');
+}else if($('#form_rooms2').html()==='4-комнатная'){
+    $('#form_rooms2').html('3-комнатная');
+}else if($('#form_rooms2').html()==='5-комнатная'){
+    $('#form_rooms2').html('4-комнатная');
+}else if($('#form_rooms2').html()==='6-комнатная'){
+    $('#form_rooms2').html('5-комнатная');
+}else if($('#form_rooms2').html()==='7-комнатная'){
+    $('#form_rooms2').html('6-комнатная');
+}else if($('#form_rooms2').html()==='8-комнатная'){
+    $('#form_rooms2').html('7-комнатная');
+}else if($('#form_rooms2').html()==='9-комнатная'){
+    $('#form_rooms2').html('8-комнатная');
+}else if($('#form_rooms2').html()==='10-комнатная'){
+    $('#form_rooms2').html('9-комнатная');
+}
+});
+
+$('#form_minus_bathrooms2').on('click', function(){
+    if($('#form_bathrooms2').html()==='2 санузла'){
+    $('#form_bathrooms2').html('1 санузел');
+}else if($('#form_bathrooms2').html()==='3 санузла'){
+    $('#form_bathrooms2').html('2 санузла');
+}else if($('#form_bathrooms2').html()==='4 санузла'){
+    $('#form_bathrooms2').html('3 санузла');
+}else if($('#form_bathrooms2').html()==='5 санузлов'){
+    $('#form_bathrooms2').html('4 санузла');
+}else if($('#form_bathrooms2').html()==='6 санузлов'){
+    $('#form_bathrooms2').html('5 санузлов');
+}
+});
+
+$('#form_plus_bathrooms2').on('click', function(){
+    if($('#form_bathrooms2').html()==='1 санузел'){
+    $('#form_bathrooms2').html('2 санузла');
+}else if($('#form_bathrooms2').html()==='2 санузла'){
+    $('#form_bathrooms2').html('3 санузла');
+}else if($('#form_bathrooms2').html()==='3 санузла'){
+    $('#form_bathrooms2').html('4 санузла');
+}else if($('#form_bathrooms2').html()==='4 санузла'){
+    $('#form_bathrooms2').html('5 санузлов');
+}else if($('#form_bathrooms2').html()==='5 санузлов'){
+    $('#form_bathrooms2').html('6 санузлов');
+}
+});
+
+/* Menu form end */
+
+/* Price menu start */
+$('.price_once').on('click', function(){
+    if($('.price_once').attr('id')!='price__menu_button_active'){
+        $('.price_once_in_week').removeAttr('id');
+        $('.price_once_in_2weeks').removeAttr('id');
+        $('.price_once_a_month').removeAttr('id');
+        $('.price_once').attr('id','price__menu_button_active')
+        $('#price_appartments_price_1rooms').html('39 BYN');
+        $('#price_appartments_price_2rooms').html('53 BYN');
+        $('#price_appartments_price_3rooms').html('67 BYN');
+    }
+});
+
+$('.price_once_in_week').on('click', function(){
+    if($('.price_once_in_week').attr('id')!='price__menu_button_active'){
+        $('.price_once').removeAttr('id');
+        $('.price_once_in_2weeks').removeAttr('id');
+        $('.price_once_a_month').removeAttr('id');
+        $('.price_once_in_week').attr('id','price__menu_button_active')
+        $('#price_appartments_price_1rooms').html('30 BYN');
+        $('#price_appartments_price_2rooms').html('41 BYN');
+        $('#price_appartments_price_3rooms').html('52 BYN');
+    }
+});
+
+$('.price_once_in_2weeks').on('click', function(){
+    if($('.price_once_in_2weeks').attr('id')!='price__menu_button_active'){
+        $('.price_once').removeAttr('id');
+        $('.price_once_in_week').removeAttr('id');
+        $('.price_once_a_month').removeAttr('id');
+        $('.price_once_in_2weeks').attr('id','price__menu_button_active')
+        $('#price_appartments_price_1rooms').html('32 BYN');
+        $('#price_appartments_price_2rooms').html('44 BYN');
+        $('#price_appartments_price_3rooms').html('56 BYN');
+    }
+});
+$('.price_once_a_month').on('click', function(){
+    if($('.price_once_a_month').attr('id')!='price__menu_button_active'){
+        $('.price_once').removeAttr('id');
+        $('.price_once_in_week').removeAttr('id');
+        $('.price_once_in_2weeks').removeAttr('id');
+        $('.price_once_a_month').attr('id','price__menu_button_active')
+        $('#price_appartments_price_1rooms').html('35 BYN');
+        $('#price_appartments_price_2rooms').html('48 BYN');
+        $('#price_appartments_price_3rooms').html('61 BYN');
+    }
+});
+
+/* Price menu end */
---
+++ js/script.js
@@ -0,0 +1,17 @@
+$(document).ready(function(){
+$('.reviews__slider').slick({
+    slidesToShow:1,
+    initialSlide: 4,
+    autoplay: true,
+    autoplaySpeed:10000,
+    waitForAnimate:false,
+    centerMode:true,
+
+});
+
+$('.header__burger').click(function(event){
+    $('.header__burger,.header__nav').toggleClass('active');
+    $('body').toggleClass('lock');
+});
+
+});
